d6e765e856511ce5c9c47a9dae406ac92ef0cffd
Evgeny Dumanov <dum_evg@mail.ru>
2023-07-16 22:42:21 +0300

    Add project files.

---
+++ Test/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.Diploma.Test
+{
+    public class BaseTest
+    {
+        public WebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            driver.Navigate().GoToUrl("https://qase.io/");
+        }
+
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Test/StartPage.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestProject1.Diploma.Test
+{
+
+    public class StartPage : BaseTest
+    {
+
+        [Test]
+        public void AddRemoveButton()
+        {
+            var expectedUrl = "https://app.qase.io/signup";
+            //   var driver = new ChromeDriver(@"D:\CD");
+            // WebDriver driver = new ChromeDriver();
+            //   WebDriver driver = new InternetExplorerDriver();
+
+            //  driver.Quit();
+
+            //  By by = By.LinkText("Add/Remove Elements");
+            driver.FindElement(By.LinkText("Start for free")).Click();
+            Assert.That(driver.Url, Is.EqualTo(expectedUrl));
+
+            //driver.FindElement(By.TagName("button")).Click();
+            //var addedButton = driver.FindElement(By.ClassName("added-manually"));
+            //Assert.IsNotNull(addedButton);
+            //addedButton.Click();
+
+            //  Assert.That(driver.FindElement(By.ClassName("added-manually")),Is.Empty);
+
+        }
+        //[Test]
+        //public void OpenGoogleWindow()
+        //{
+        //    driver.Navigate().GoToUrl("https://google.com/");
+        //}
+
+    }
+}
